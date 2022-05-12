a37ed7d5a1bb75dccf93ddf73e42d46626f874a0
Franco Morales Castro <franco.morales@outlook.com>
2022-05-11 21:30:20 -0400

    Eliminar registro tabla

--- clase4/js/main.js
+++ clase4/js/main.js
@@ -9,13 +9,15 @@ usersList.push({
 const getUsers = () => {
     let htmlOut = '<table class="table">';
     htmlOut = htmlOut + '<tr>';
-    htmlOut = htmlOut + '<th>Email</th><th>First name</th><th>Last name</th>';
+    htmlOut = htmlOut + '<th>Email</th><th>First name</th><th>Last name</th><th></th><th></th>';
     htmlOut = htmlOut + '</tr>';
     usersList.forEach(item => {
         htmlOut = htmlOut + '<tr>';
         htmlOut = htmlOut + `<td>${item.email}</td>`;
         htmlOut = htmlOut + `<td>${item.firstName}</td>`;
         htmlOut = htmlOut + `<td>${item.lastName}</td>`;
+        htmlOut = htmlOut + `<td><input type="button" class="btn btn-warning" value="edit" /></td>`;
+        htmlOut = htmlOut + `<td><input type="button" class="btn btn-danger" value="delete" onclick="deleteUser('${item.email}')" /></td>`;
         htmlOut = htmlOut + '</tr>';
     });
     htmlOut = htmlOut + '</table>';
@@ -32,10 +34,40 @@ const getUsersDestructuring = () => {
 }
 
 const addUser = (email, firstName, lastName) => {
-    usersList.push({
-        email,
-        firstName,
-        lastName
-    });
-    getUsers();
+    if(indexUser(email) !== -1) {
+        alert('El email ingresado ya se encuentra registrado.')
+    }
+    else {
+        usersList.push({
+            email,
+            firstName,
+            lastName
+        });
+        getUsers();
+    }
+}
+
+const deleteUser = (email) => {
+    const index = indexUser(email);
+    if(index === -1) {
+        alert('El usuario no existe')
+    } else {
+        if(confirm('Esta seguro?') === true) {
+            usersList.splice(index);
+            getUsers();
+        }
+    }
+}
+
+const indexUser = (email) => {
+    let index = -1;
+    let indexCount = 0;
+    usersList.forEach(item => {
+        if(item.email === email) index = indexCount;
+        indexCount++;
+    })
+    return index;
+
+    // Esta nos retorna un valor boolean true cuando encuentra un valor
+    // return usersList.some(item => item.email === email);
 }
